c80459768e0779055567521fb131c3d8b3ed2d0b
Michael Smith <5875452+themikesmith@users.noreply.github.com>
2026-01-28 20:57:02 -0500

    Update DemoConfig.cs
    add     public new bool DateDayBeforeMonth => true;

--- WeddingWebsite/Config/ThemeAndLayout/DemoConfig.cs
+++ WeddingWebsite/Config/ThemeAndLayout/DemoConfig.cs
@@ -17,6 +17,7 @@ public class DemoConfig : DefaultConfig, IWebsiteConfig
     public new IEnumerable<string> IgnoredValidationIssues => [
         "You have both a Timeline and SimpleTimeline section. Since both sections display the same information, choose the level of detail you want and remove the other section."
     ];
+    public new bool DateDayBeforeMonth => true;
 
     public DemoConfig() {
         var surfaceVariant = new Colour(254, 252, 231);
