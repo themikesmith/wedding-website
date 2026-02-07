77a73d624951e9e60473299826a3e6064dd75cad
mcs <test@test.com>
2026-02-07 15:40:48 -0500

    enable Links to have DisplayText, add handling to convert to LinkButton

--- WeddingWebsite/Models/WebsiteElement/WebsiteLink.cs
+++ WeddingWebsite/Models/WebsiteElement/WebsiteLink.cs
@@ -1,5 +1,21 @@
 ﻿namespace WeddingWebsite.Models.WebsiteElement;
 
-public record WebsiteLink(
-    string Url
-);
+public class WebsiteLink(string url, string? displayText = null) : IWebsiteElement
+{
+    public string Url { get; } = url;
+    public string? DisplayText { get; } = displayText ?? url;
+
+    public WebsiteLink(string url) : this(url, null) {}
+    
+    public string GetHtml(string classList = "") {
+        if (DisplayText != null) {
+            return $"<a href={Url}>{DisplayText}</a>";
+        }
+        else {
+            return $"<a href={Url}>{Url}</a>";
+        }
+    }
+    public override string ToString() {
+        return Url;
+    }
+}
