1bdda0a2e481d5497c1e91abb0bf8beb041822ee
mcs <test@test.com>
2026-02-07 15:39:30 -0500

    handle runat server for WebsiteImages, allow local serving of files

--- WeddingWebsite/Models/Gallery/GalleryItem.cs
+++ WeddingWebsite/Models/Gallery/GalleryItem.cs
@@ -2,10 +2,11 @@
 
 namespace WeddingWebsite.Models.Gallery;
 
-public record GalleryItem(WebsiteImage Image, int Size = 1)
+public record GalleryItem(WebsiteImage Image, int Size = 1, bool runAtServer = false)
 {
     /// <summary>
     /// Create a gallery item with a URL image and no alt text.
     /// </summary>
-    public GalleryItem(string url) : this(new WebsiteImage(url, null)) {}
+    public GalleryItem(string url) : this(new WebsiteImage(url, null, false)) {}
+    public GalleryItem(string url, bool runatServer) : this(new WebsiteImage(url, null, runatServer)) {}
 }
--- WeddingWebsite/Models/WebsiteElement/WebsiteImage.cs
+++ WeddingWebsite/Models/WebsiteElement/WebsiteImage.cs
@@ -1,21 +1,31 @@
 ﻿namespace WeddingWebsite.Models.WebsiteElement;
 
-public class WebsiteImage(string url, string? altText, IEnumerable<ImageSizeVariation> customSizes) : IWebsiteElement
+public class WebsiteImage(string url, string? altText, IEnumerable<ImageSizeVariation> customSizes, bool runatServer) : IWebsiteElement
 {
     public string Url { get; } = url;
     public string? AltText { get; } = altText;
     public IEnumerable<ImageSizeVariation> CustomSizes { get; } = customSizes;
+    // <asp:Image ID="Image1" runat="server" ImageUrl="~/HandlerCS.ashx?FileName=Penguins.jpg" />
+    public bool RunAtServer { get; } = runatServer;
     
-    public WebsiteImage(string url, string? altText) : this(url, altText, []) {}
+    public WebsiteImage(string url, string? altText) : this(url, altText, [], false) {}
+    public WebsiteImage(string url, string? altText, bool runAtServer) : this(url, altText, [], runAtServer) {}
 
     public string GetHtml(string classList = "") {
+        var optional_server_param = "";
+        var tag_class = "img";
+        if(RunAtServer) {
+            optional_server_param = "runat=\"server\" ";
+            //tag_class = "asp:Image";
+            tag_class = "img";
+        }
         if (CustomSizes.Any())
         {
             return $"<picture class=\"{classList}\">" +
                    string.Join("", CustomSizes.Select(size => $"<source srcset=\"{size.Src}\" media=\"(width >= {size.MinWidth}px)\">")) +
-                   $"<img class=\"{classList}\" src=\"{Url}\" alt=\"{AltText ?? ""}\" />" +
+                   $"<{tag_class} class=\"{classList}\" {optional_server_param}src=\"{Url}\" alt=\"{AltText ?? ""}\" />" +
                    $"</picture>";
         }
-        return $"<img class=\"{classList}\" src=\"{Url}\" alt=\"{AltText ?? ""}\" />";
+        return $"<{tag_class} class=\"{classList}\" {optional_server_param}src=\"{Url}\" alt=\"{AltText ?? ""}\" />";
     }
 }
