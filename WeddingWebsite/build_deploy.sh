eaf8a385c3dbcacf939808288b0139d969a2b871
mcs <test@test.com>
2026-03-09 23:41:29 -0400

    start and stop docker in deploy scripot

--- WeddingWebsite/build_deploy.sh
+++ WeddingWebsite/build_deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+sudo systemctl start docker
+
 sudo docker build -t weddingwebsite .
 
 sudo docker tag weddingwebsite registry.digitalocean.com/octopus-containers/weddingwebsite
 
 sudo docker push registry.digitalocean.com/octopus-containers/weddingwebsite
 
-
+sudo systemctl stop docker
